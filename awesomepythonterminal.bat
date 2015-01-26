c77613c0a74e6ea2791b6083811febe4aa8a0798
Cosmic Open Source Projects <rodeboys5@gmail.com>
2015-01-26 17:57:43 -0500

    Create awesomepythonterminal.bat

---
+++ awesomepythonterminal.bat
@@ -0,0 +1,9 @@
+::Made with https://github.com/ilovecode1/launchpythonwithbatchdemo
+
+echo off
+set /p pathName=Enter Python Version Example: if python version is 3.2 type 32!
+echo Starting!
+start c:\python%versionnum%\python awesomepythonterminal.py
+echo Boot is done...
+PAUSE
+END
