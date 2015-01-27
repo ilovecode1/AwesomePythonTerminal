52740d8f75a16f5b899f75afbcaa0f58d2e0246f
CosmicWebServices <andrewrodebaugh@cosmicsearch.org>
2015-01-27 16:56:29 -0500

    Version 0.1.5

--- awesomepythonterminal.py
+++ awesomepythonterminal.py
@@ -25,7 +25,7 @@ while running == True:
         print("-----------------------------------------------------------------")
         help
     
-    else:
+    if command != "exit" or "help":
         
         exec command
     
